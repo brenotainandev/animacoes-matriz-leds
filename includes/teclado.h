893a4cc6871b84cd864613f646effcf144a8edfe
Breno Tainan Aguiar <brenotaguiar@gmail.com>
2025-01-26 21:51:24 -0300

    movendo funcao de ler teclado para arquivo teclado.c

---
+++ includes/teclado.h
@@ -0,0 +1,15 @@
+#ifndef TECLADO_H
+#define TECLADO_H
+
+#include <stdint.h>
+
+// Configuração do teclado matricial
+extern char teclado[4][4];          // Mapeamento das teclas
+extern int pinos_colunas[4];        // Pinos das colunas do teclado
+extern int pinos_linhas[4];         // Pinos das linhas do teclado
+
+// Funções para inicializar e ler o teclado
+void inicializar_teclado();         // Inicializa os pinos do teclado
+char ler_teclado();                 // Lê a tecla pressionada
+
+#endif // TECLADO_H
